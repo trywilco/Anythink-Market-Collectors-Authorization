ce1dccf2f9a24d1418abe72a8c458a64078345bf
Eti Noked <42963541+EtiNoked@users.noreply.github.com>
2023-09-10 17:50:03 +0300

    Update sessions_controller.rb

--- .framework/rails/backend/app/controllers/sessions_controller.rb
+++ .framework/rails/backend/app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters
+
   def create
     user = User.find_by(email: sign_in_params[:email])
 
@@ -9,5 +11,12 @@ class SessionsController < Devise::SessionsController
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit :username in addition to :email and :password
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :username])
   end
 end
